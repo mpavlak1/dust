24edc6ab3e5bc1a1576a39aecfac2b798c44e826
mpavlak1 <michael.m.pavlak.civ@mail.mil>
2020-01-21 11:30:23 -0500

    need to make sure subprocess drive has tesseract
    default sub process drive not always the same as the drive tesseract installed on. Need to change sub process directory to the same drive.
    
    If tesseract install on C: and default drive is Q:, need to switch to C:, call subprocess, then return to Q:

--- src/utils/spawn.py
+++ src/utils/spawn.py
@@ -1,6 +1,7 @@
 # /dust/src/utils/spawn.py
 
 # Built-ins
+import os
 import subprocess
 from subprocess import BELOW_NORMAL_PRIORITY_CLASS
 from subprocess import CREATE_NO_WINDOW
@@ -11,10 +12,19 @@ from dust.src.errors import NonZeroExitCodeError
 
 
 def call_nowindow(args):
+    __wd__ = os.getcwd()
+    os.chdir('C:')
     if not args: return 0
     exit_code = subprocess.call(args, creationflags = call_nowindow.flag)
-    if(exit_code != 0):
-        raise NonZeroExitCodeError('Exited with non-zero exit-code {}'.format(exit_code))
-    return exit_code
+
+    try:
+        if(exit_code != 0):
+            raise NonZeroExitCodeError('Exited with non-zero exit-code {}'.format(exit_code))
+        return exit_code
+    except Exception as e:
+        raise e
+    finally:
+        os.chdir(__wd__)
+
 call_nowindow.flag = BELOW_NORMAL_PRIORITY_CLASS + CREATE_NO_WINDOW
 
